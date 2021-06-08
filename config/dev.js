e6d399e0172dde0851f95da85ccad73e52a36cc8
unknown <shubhamdudhal9@gmail.com>
2021-06-09 02:03:01 +0530

    updated config and mongo

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://admin:admin@cluster0.idv1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+}
--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const mongoConnection = 'mongodb+srv://admin:admin@cluster0.idv1i.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-mongoose.connect(mongoConnection,
-{   useNewUrlParser:true,
-    useUnifiedTopology:true}).then(()=>console.log("connected"))
+const config = require('./config/key')
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const {User}= require('./models/user');
+mongoose.connect(config.mongoURI,
+{   useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>console.log("Database connected"))
                        .catch(err=>console.error(err));
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+
+app.post('/api/user/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err,userData)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+        })
+    })
+    
+})
+
+
+
 app.get('/',(req,res)=>{
     res.send('Hello World');
     console.log("working");
