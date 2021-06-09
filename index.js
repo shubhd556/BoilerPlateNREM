680110d85fc200a2bea03536dbc8db3ad10d55cd
unknown <shubhamdudhal9@gmail.com>
2021-06-10 00:13:50 +0530

    Index js models

--- index.js
+++ index.js
@@ -5,36 +5,77 @@ const config = require('./config/key')
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const {User}= require('./models/user');
-mongoose.connect(config.mongoURI,
-{   useNewUrlParser:true,useUnifiedTopology:true,useCreateIndex:true}).then(()=>console.log("Database connected"))
-                       .catch(err=>console.error(err));
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({
+const connect = mongoose.connect(config.mongoURI,
+    {
+      useNewUrlParser: true, useUnifiedTopology: true,
+      useCreateIndex: true, useFindAndModify: false
+    })
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
+
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cookieParser());
 
-app.post('/api/user/register',(req,res)=>{
-    const user = new User(req.body)
-    user.save((err,userData)=>{
-        if(err) return res.json({success:false,err})
-        return res.status(200).json({
-            success:true
-        })
-    })
+app.get('/api/user/auth',(req,res)=>{
     
 })
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Hello World');
-    console.log("working");
+
+
+
+app.post('/api/user/register',(req,res)=>{
+    const user = new User(req.body)
+    user.save((err,doc)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true,
+            userData:doc
+        })
+    })
+    
+})
+
+app.post("/api/user/login", (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user)
+            return res.json({
+                loginSuccess: false,
+                message: "Auth failed, email not found"
+            });
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch)
+                return res.json({ loginSuccess: false, message: "Wrong password" });
+
+        user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err);
+                res.cookie("w_authExp", user.tokenExp);
+                res
+                    .cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true, userId: user._id
+                    });
+            });
+        });
+    });
 });
 
 
-app.listen(5000);
+
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+  console.log(`Server Listening on ${port}`)
+});
 
